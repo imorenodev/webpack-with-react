d0ce3cfbb332d194ad463a5fe34e4420ded41f09
imorenodev <imorenodev@gmail.com>
2016-02-22 13:00:47 -0500

    fixed hotmoduleloader issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
-const merge = require("webpack-merge");
 const webpack = require("webpack");
+const merge = require("webpack-merge");
+const NpmInstallPlugin = require("npm-install-webpack-plugin");
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, "app"),
@@ -14,18 +15,29 @@ const common = {
   output: {
     path: PATHS.build,
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
   }
 };
 
 //Default configuration
 if (TARGET === "start" || !TARGET) {
   module.exports = merge(common, {
+    devtool: "eval-source-map",
     devServer: {
       contentBase: PATHS.build,
 
       //Enable history API fallback so HTML5 History API-base routing works for default
       historyApiFallback: true,
-      hot: true,
+      //using --hot AND HotModuleReplacementPlugin breaks the feature...
+      //hot: true
       inline: true,
       progress: true,
 
