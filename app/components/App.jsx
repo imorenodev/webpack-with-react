c1db70c94c0a840bc45d534fe94a0ec5a6b20a3b
imorenodev <imorenodev@gmail.com>
2016-02-22 14:38:55 -0500

    removed app/component.js

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Note from "./Note.jsx";
 
-export default class App extends React.component {
-  render () {
+export default class App extends React.Component {
+  render() {
     return <Note />;
   }
 }
