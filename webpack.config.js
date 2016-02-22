c570ab19b466068dac3b76298697c5284589a2a9
imorenodev <imorenodev@gmail.com>
2016-02-22 12:16:34 -0500

    added HotModuleReplacement to dev server env

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const merge = require("webpack-merge");
+const webpack = require("webpack");
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, "app"),
@@ -18,7 +19,27 @@ const common = {
 
 //Default configuration
 if (TARGET === "start" || !TARGET) {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      //Enable history API fallback so HTML5 History API-base routing works for default
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      //Display only errors, to reduce amount of output
+      stats: "errors-only",
+
+      //Parse host and port from env so this is easy to customize
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
 }
 if (TARGET === "build") {
   module.exports = merge(common, {});
