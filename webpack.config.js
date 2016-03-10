d0ef06448198017705559dae46beff5f220d43bc
imorenodev <imorenodev@gmail.com>
2016-03-10 08:10:03 -0500

    setting up ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
@@ -15,12 +16,22 @@ const common = {
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+      test: /\.css$/,
+      loaders: ['style', 'css'],
+      include: PATHS.app
+      }
+    ]
   }
 };
 
 //Default configuration
 if (TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
+    devtool: 'eval-source-map',
     devServer: {
       contentBase: PATHS.build,
       historyApiFallback: true,
@@ -32,7 +43,10 @@ if (TARGET === 'start' || !TARGET) {
       port: process.env.PORT
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save / -S
+      })
     ]
   });
 }
