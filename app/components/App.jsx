b3b86df47a62a82b230e0739321d9be5026154aa
imorenodev <imorenodev@gmail.com>
2016-03-10 16:27:32 -0500

    added notes list to App component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,38 @@
+import uuid from 'node-uuid';
 import React from 'react';
 import Note from './Note.jsx';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Make Dinner'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    }
+  }
   render() {
-    return <Note />;
+    const notes = this.state.notes;
+    return (
+      <div className='container'>
+        <li className='list-group-item active'>Task Items List:</li>
+        <ol className='list-group'>
+          {notes.map(note =>
+          <li className='list-group-item' key={note.id}>{note.task}</li>)}
+        </ol>
+      </div>
+    );
   }
 }
