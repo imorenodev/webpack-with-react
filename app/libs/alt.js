139ffdd2a078672bb54eb3baff9855eb49ede648
imorenodev <imorenodev@gmail.com>
2016-03-12 13:33:43 -0500

    Installed alt to project

---
+++ app/libs/alt.js
@@ -0,0 +1,8 @@
+import Alt from 'alt';
+// Uncomment the lines below to debug alt in chrome using Alt-devtool chrome extension
+//import chromeDebug from 'alt-utils/lib/chromeDebug';
+
+const alt = new Alt();
+//chromeDebug(alt);
+
+export default alt;
