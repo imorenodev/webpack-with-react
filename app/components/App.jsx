072418695759705af7c1921eb5e7dfc673e58347
imorenodev <imorenodev@gmail.com>
2016-03-14 12:21:29 -0400

    Implement AltContainer to manage state in App component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,37 +2,23 @@ import React from 'react';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
+import AltContainer from 'alt-container';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = NoteStore.getState();
-  }
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-  componentWillUnmount() {
-    NoteStore.unlisten(this.storeChanged);
-  }
-  storeChanged = (state) => {
-    // Without a property initializer, 'this' wouldn't point to the correct
-    // context because it defaults to undefined in strict mode.
-    this.setState(state);
-  }
+  // AltContainer allows us to bind data to its immediate children.
+  // In this case, it injects the notes property into Notes.
   render() {
-    //property initializer
-    const notes = this.state.notes;
-
     return (
       <div className='container'>
         <li className='list-group-item active'>
           <button onClick={this.addNote} className='btn btn-success btn-sm float-left'>+</button>
           <span>Todo List</span>
         </li>
-        <Notes notes={notes}
-               onEdit={this.editNote}
-               onDelete={this.deleteNote} />
+        <AltContainer stores={[NoteStore]}
+                      inject={{notes: () => NoteStore.getState().notes}}>
+          <Notes onEdit={this.editNote}
+                 onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
     );
   }
